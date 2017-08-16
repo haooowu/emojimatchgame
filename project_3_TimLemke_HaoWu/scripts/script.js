8c90e70cfeee18c0d74ab031ee7c6b3da951905f
Hao Wu <wh1993123@hotmail.com>
2017-08-16 01:19:42 -0400

    Add files via upload
    flip logic and minor fix

--- project_3_TimLemke_HaoWu/scripts/script.js
+++ project_3_TimLemke_HaoWu/scripts/script.js
@@ -1,14 +1,16 @@
+var emojiSet = [];
+var counter = 0;
+var compare = [];
+
 $(function() {
-	var emojiSet = [];
 	//grab emoji from 😀 up to 🙄, total 69 emojis faces
 	for (var i = 0x1F600; i <= 0x1F644; i += 1){
-		// console.log(String.fromCodePoint(i));
 		emojiSet.push(String.fromCodePoint(i));
 	}
 	/** Docstring **
 	* Randomly select unique emojis from the list by shuffle it first and slice the number
-	* @param  {array[]} array that needs to be shuffled.
-	* @param  {int} number of emoji needs to slice depends on the level.
+	* @param {array[]} array that needs to be shuffled.
+	* @param {int} number of emoji needs to slice depends on the level.
 	* @return {array[]}
 	*/
 	function randomSelect(array, num) {
@@ -25,58 +27,91 @@ $(function() {
 		}
 		return array.slice(0, num);
 	}
-
 	var randomSelected = randomSelect(emojiSet, 8);
-	// console.log(randomSelect(emojiSet, 4));
 	var gameSample = [];
-		for(var i = 0; i< randomSelected.length; i+=1){
-			gameSample.push(randomSelected [i]);
-			gameSample.push(randomSelected [i]);
-		}
+	for(var i = 0; i< randomSelected.length; i+=1){
+		gameSample.push(randomSelected [i]);
+		gameSample.push(randomSelected [i]);
+	}
 	//shuffle the duplicated board
 	var finalBoard = randomSelect(gameSample, gameSample.length);
-	//<li class="emoji tile">${finalBoard[i]}</li>
 	for (var i = 0; i < finalBoard.length; i++) {
-		$('#board').append($(`<div class="emojicard"><div class="front"> ?</div><div class="back"><li class="emoji">${finalBoard[i]}</li></div></div>`));
+		$('#board').append($(`<li class="emojicard"><div class="front">?</div><div class="back">${finalBoard[i]}</div></li>`));
 	}
-
-	$('.emojicard').on('click', function() {
-		$(this).addClass("flip");
-	});
-
-	// for (var i = 0; i < finalBoard.length; i++) {
-	// 	$('#board').append($(`<div class="card emojiwrapper"><div class="front"> ?</div><div class="back"><li class="emoji">${finalBoard[i]}</li></div></div>`));
-	// }
-	//clip handler with plugin
-	//https://nnattawat.github.io/flip/#back
-	// $(".card").flip()
-	// var flag = true;
-	// $(".card").on('flip:done',function(){
-	// 	if (flag){
-	// 		$(this).flip(true);
-	// 		//triggger timer function
-	// 		console.log(flag);
-	// 		flag = false;
-	// 	} else {
-	// 		$(".card").flip(false);
-	// 		flag = true;
-	// 	}
-	// });
-	/** Docstring **
-	* When first one is being clicked, tirgger timer
-	* First card stay at flip, second one will determine match
-	* if ===, both stays , score ++
-	* if not, both flip back
+	//onClick listener
+	/**
+	* TODO: Trigger timer when counter is 1
+	* TODO: Trigger alertbox when fisinsh, or triiger it when timeout
 	*/
-
-	var count = 60;
-	//https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
-	var countdown = setInterval(function() {
-		count = count - 1;
-		console.log(count);
-		if (count === 0) {
-			clearInterval(countdown);
+	$("ul").on("click", "li", function(){
+		var $current = ($(this));
+		// if the li has not been flipped
+		if (!$current.hasClass("flip")) {
+			// creating an object that consist of the li's index and the value then add to an array
+			var emoInfo = {"index": $current.index(),"value":$current.find(".back").text()}
+			compare.push(emoInfo);
+			counter += 1;
+			$current.addClass("flip");
+			// if this is the second one:
+			if (counter == 2){
+				//locks other li
+				$("li").addClass("off");
+				// if their value are the same
+				if (compare[0].value === compare[1].value){
+					//look for the previous' index and let them stay
+					var preIndex = compare[0].index;
+					$current.addClass("correct");
+					$current.parent().children().eq(preIndex).addClass("correct");
+					//initialize
+					counter = 0;
+					compare = [];
+					$("li").removeClass('off');
+					setTimeout(function() {
+						// wait for fliping to finish
+						if ($("#board > li.correct").length == 16){
+							alert("you win");
+							$("li").removeClass('flip');
+						}
+					}, 350);
+				} else {
+					// wait a bit for displaying
+					setTimeout(function() {
+						$("li").removeClass('flip off');
+					}, 600);
+					//initialize
+					compare = [];
+					counter = 0;
+				}
+			}
 		}
-	}, 1000);
-
+	});
+	console.log($("#board > li.correct").length)
 });
+
+/* clip() handler with jQuery plugin (https://nnattawat.github.io/flip/#back)
+// $(".card").flip()
+// var flag = true;
+// $(".card").on('flip:done',function(){
+// 	if (flag){
+// 		$(this).flip(true);
+// 		//triggger timer function
+// 		console.log(flag);
+// 		flag = false;
+// 	} else {
+// 		$(".card").flip(false);
+// 		flag = true;
+// 	}
+// });
+
+/** Docstring **
+* simple counter
+*/
+// var count = 60;
+// //https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+// var countdown = setInterval(function() {
+// 	count = count - 1;
+// 	console.log(count);
+// 	if (count === 0) {
+// 		clearInterval(countdown);
+// 	}
+// }, 1000);
